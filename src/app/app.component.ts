9e68baa56a97ae783c4467ca61188f01a8fd0980
gio911 <kge91186@mail.ru>
2021-01-06 00:48:16 +0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,6 @@ export class AppComponent  {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
+Use of this source code is governed by an MIT-style license that ppp
 can be found in the LICENSE file at https://angular.io/license
-*/cvcv
+*/
